62833193e88edca80ce3010adf4edc91c19218b5
nusrat jahan <nusrat.jahan@dsinnovators.com>
2022-10-20 15:38:21 +0600

    Dtabase connection successfully

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
-app.get('/', (req, res)=>{
-    res.send('Hello world');
-})
 
+// database connection
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database connection successful!"))
+  .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
+app.get("/alien", (req, res) => {
+  const id = req.query.id;
+  res.send("Welcome back to alien" + id);
+});
 
-
-
+app.get("/alien/:id", (req, res) => {
+  const id = req.params.id;
+  res.send("hey jinia " + id);
+});
 
 app.listen(9000, function (req, res) {
   console.log("Server running");
